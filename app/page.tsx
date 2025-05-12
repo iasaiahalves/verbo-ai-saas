6eb66a17d5fd2f072e26c382cc9415df64b93078
ICE <alvesiasaiah@gmail.com>
2025-05-12 20:13:42 +0800

    feat: add DemoSection and HowItWorksSection components; update Home component to include new sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import BgGardient from "@/components/common/bg-gradient";
+import DemoSection from '@/components/home/demo-section';
 import HeroSection from '@/components/home/hero-section';
+import HowItWorksSection from '@/components/home/how-it-works-section';
+
+
 
 export default function Home() {
   return (
     <div className="relative w-full">
       <BgGardient />
       <div className="flex flex-col ">
-         <HeroSection />
+        <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
       </div>
      
-      {/* <DemoSection /> */}
-      {/* <HowItWorksSection /> */}
+      
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
 
